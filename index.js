664aa323cdc82bc45014eb51cd48c4ae15660455
Muhammad Ahmad <93067257+MuhammadAhmadAk@users.noreply.github.com>
2025-05-25 11:57:36 -0700

    Update and deploy Firebase Function

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// functions/index.js
-
 const functions = require('firebase-functions');
 const express = require('express');
 const { sendNotification } = require('./fcm_service');
-const app= express();
+
+const app = express();
 app.use(express.json());
+
 app.post('/sendNotification', async (req, res) => {
     const { deviceToken, title, body, data } = req.body;
     const result = await sendNotification(deviceToken, title, body, data);
     res.json(result);
 });
-app.listen(8000, () => {
-    console.log('Server is running on port 3000');
-}
-);
+
+// Export the Express app as a Firebase Function
+exports.api = functions.https.onRequest(app);
